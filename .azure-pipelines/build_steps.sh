0f35ae601b2a4706237d43af41e98c1d7cd92f0e
Maksim Rakitin <mrakitin@bnl.gov>
2019-09-21 17:15:41 -0400

    yum install alsa-lib for libasound.so.2

--- .azure-pipelines/build_steps.sh
+++ .azure-pipelines/build_steps.sh
@@ -31,7 +31,7 @@ source run_conda_forge_build_setup
 # "recipe/yum_requirements.txt" file. After updating that file,
 # run "conda smithy rerender" and this line will be updated
 # automatically.
-/usr/bin/sudo -n yum install -y mesa-libGL libXcomposite libXcursor libXi libXtst libXScrnSaver libXrandr
+/usr/bin/sudo -n yum install -y mesa-libGL libXcomposite libXcursor libXi libXtst libXScrnSaver libXrandr alsa-lib
 
 
 # make the build number clobber
